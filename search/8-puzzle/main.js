5f8e6441c25f81c41c529cca571af46fc4784d67
renanjpaula <renanjohannsen@hotmail.com>
2017-04-21 21:55:40 +0000

    implementações DFS e BFS

--- search/8-puzzle/main.js
+++ search/8-puzzle/main.js
@@ -2,15 +2,16 @@
 
 const State = require('./state')
 const Bfs = require('./bfs')
+const Dfs = require('./dfs')
 
 const initialState = new State([[1, 2, 3],
                                 [4, 0, 5],
                                 [8, 6, 7]])
 
-const solutionState = new State([[1, 2, 3],
-                                 [4, 0, 5],
+const solutionState = new State([[0, 1, 2],
+                                 [3, 4, 5],
                                  [6, 7, 8]])
 
-const solver = new Bfs(solutionState)
+const solver = new Dfs(solutionState)
 solver.solve(initialState)
 solver.print()
